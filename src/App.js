6c5e59d0916874afa49c2f7fb2df50ba277ea8e0
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2022-09-15 14:13:19 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const title = "Welcome To My App";
+    const age = 20;
+    const link = "https://maribayaeleven.com";
+
+    return (
+        <div>
+            <h1>{ title }</h1>
+            <h1>{ age * 2 }</h1>
+            <a href={link}>Go To My Website</a>
+        </div>
+    );
 }
 
 export default App;
